25db1a10fd802f7395a275b159600fe52fec5198
tilley14 <nikolas.tilley@gmail.com>
2025-03-14 22:26:04 -0400

    Adding todos

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,16 @@ int main(int argc, char* argv[]) {
     using namespace chipp8;
     chip8 cpu;
     init(cpu);
+
+    // What is the clock rate?
+    // TODO: emulate a clock source
+    while (true) {
+        // check for user input
+        // cpu get current instruction
+        // execute instruction
+        // increment pc
+        // draw display
+    }
+
     return 0;
 }
